50d8c68013dc5b2aa60cc583114bff48e544425b
Nicholas Blott <blottn@tcd.ie>
2018-03-20 19:10:15 +0000

    added getting all text fields that are flagged as encrypted

--- chrome-ext/racl.js
+++ chrome-ext/racl.js
@@ -1,2 +1,22 @@
+const tag = "*ROBOT VOICE* BEGIN ENCRYPTED TEXT *END ROBOT VOICE*"
 
-alert("hello world");
+function isEncrypted(txt) {
+	return txt.substring(txt.length - tag.length,txt.length) === tag;
+}
+
+function getEncrypted(txt) {
+	return txt.substring(0,txt.length - tag.length);
+}
+
+document.onload = new function() {
+	var dom_list = document.getElementsByTagName("P");
+	var list = []
+	for (var i = 0 ; i < dom_list.length ; i++) {
+		var txt = dom_list[i].innerText;
+		if (txt != "" && txt.length >= tag.length && isEncrypted(txt)) {
+			console.log(getEncrypted(txt));
+			list.push(getEncrypted(txt));	//cant write further without writing the encryption side first
+		}
+	}
+
+}
