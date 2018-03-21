040f8e31dd2bc2d37805740c1878c0d656533d60
Nicholas Blott <blottn@tcd.ie>
2018-03-21 16:33:40 +0000

    removed functions for asymmetric keys and example key

--- key_svr/server/urls.py
+++ key_svr/server/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^example_key/',views.example_key),
+    url(r'^get_pubk/',views.get_pubk),
 ]
--- key_svr/server/views.py
+++ key_svr/server/views.py
@@ -5,41 +5,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import sys, os
 
-public_keys = {}
+keys = {}
 
-groups = {}
 
 E_MSG = 'na'
 
 def get_pubk(request):
     gid = request.GET['gid']
     if gid in public_keys.keys():
-        return HttpResponse(public_keys[groupid])
+        return HttpResponse(public_keys[gid]['aes_key'])
     else:
-        return HttpResponse("na")
+        return HttpResponse(E_MSG)
 
-
-def get_privk(request):
-    if not "gid" in request.GET['gid']:
-        return HttpResponse('gid required')
-    if not "uid" in request.GET['uid']:
-        return HttpResponse('uid required')
-    groupid = request.GET['gid']
-    if gid in public_keys.keys() and authorised(request):
-        return HttpResponse(public_keys[groupid])
-    else:
-        return HttpResponse("na")
 
 def authorised(uid, groupid):
-    return groupid in groups.keys() and uid in groups[groupid]["members"]
-
-def example_key(request):
-    pub = open('./trimmed')
-    priv = open('./trimmed.pub')
-    pub_key = ''
-    priv_key = ''
-    for line in pub:
-        pub_key += line
-    for line in priv:
-        priv_key += line
-    return HttpResponse('{\"pub_key\":\"' + pub_key + '\", \"priv_key\":\"' + priv_key + '\"}')
+    return uid in groups.keys() and uid in groups[groupid]["members"]
