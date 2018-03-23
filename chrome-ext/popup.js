6aad63f336c6daaf0891229a05f9b5b148c84a04
Nicholas Blott <blottn@tcd.ie>
2018-03-23 21:30:35 +0000

    added login browser action, still requires loging in function

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -1,14 +1,22 @@
-
 document.onload = new function() {
-	chrome.storage.local.set({"x":"why  hi there"}, new function() {
-		chrome.storage.local.get(["x"],new function(res) {
-			alert("result: " + res.key);
+	chrome.storage.sync.set({'uid_k':'example'}, function () {
+		chrome.storage.sync.get(['uid_k'],function (result) {	//check uid
+			//get elements
+			var login_div = document.getElementById('login');
+			var status_div = document.getElementById('status');
+			var text = document.getElementById('text');
+
+			if (!(typeof result['uid_k'] == 'undefined' || result['uid_k'] === '')) {	//logged in
+				login_div.style.display = 'none';
+				status_div.style.display = 'block';
+				text.innerHTML += result['uid_k'];
+			}
+			else {
+				// show login
+				login_div.style.display = 'block';
+				status_div.style.display = 'none';
+			}
 		});
 	});
 }
-
-
-/*chrome.sync.get("x",new function(res) {
-		alert("result" + res);
-});*/
 
