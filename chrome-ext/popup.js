5baaa59b357c4d490c80ac70f8d7bb413c91cbcd
Nicholas Blott <blottn@tcd.ie>
2018-03-26 14:44:12 +0100

    completed login page

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -1,22 +1,38 @@
-document.onload = new function() {
-	chrome.storage.sync.set({'uid_k':'example'}, function () {
-		chrome.storage.sync.get(['uid_k'],function (result) {	//check uid
-			//get elements
-			var login_div = document.getElementById('login');
-			var status_div = document.getElementById('status');
-			var text = document.getElementById('text');
+function login() {
+	var val = document.getElementById('name').value;
+	chrome.storage.sync.set({'uid_k':val}, login_check);
+}
 
-			if (!(typeof result['uid_k'] == 'undefined' || result['uid_k'] === '')) {	//logged in
-				login_div.style.display = 'none';
-				status_div.style.display = 'block';
-				text.innerHTML += result['uid_k'];
-			}
-			else {
-				// show login
-				login_div.style.display = 'block';
-				status_div.style.display = 'none';
-			}
-		});
+function logout() {
+	chrome.storage.sync.set({'uid_k':''}, login_check);
+}
+
+function login_check() {
+	chrome.storage.sync.get(['uid_k'],function (result) {	//check uid
+		//get elements
+		var login_div = document.getElementById('login_div');
+		var status_div = document.getElementById('status');
+		var text = document.getElementById('text');
+
+		if (!(typeof result['uid_k'] == 'undefined' || result['uid_k'] === '')) {	//logged in
+			login_div.style.display = 'none';
+			status_div.style.display = 'block';
+			text.innerHTML = 'Logged in as: ';
+			text.innerHTML += result['uid_k'];
+		}
+		else {
+			// show login
+			login_div.style.display = 'block';
+			status_div.style.display = 'none';
+			text.innerHTML = 'Logged in as: ';	//clear current login text
+		}
 	});
+
+}
+
+window.onload = function() {
+	document.getElementById('login').onclick = login;
+	document.getElementById('logout').onclick = logout;
+	login_check();
 }
 
