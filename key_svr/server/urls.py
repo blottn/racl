c4726a4e1dddfe3750e65d0d40557fa9d4889218
Nicholas Blott <blottn@tcd.ie>
2018-03-27 12:27:07 +0100

    scrapped everything, moving to different data structure

--- key_svr/server/urls.py
+++ key_svr/server/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^get_pubk/',views.get_pubk),
+	url(r'^register/',views.register),
 ]
--- key_svr/server/views.py
+++ key_svr/server/views.py
@@ -3,33 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+
 import sys, os
 
 keys = {}
 
-
 E_MSG = 'na'
 
-def get_pubk(request):
-    gid = request.GET['gid']
-    if gid in public_keys.keys():
-        return HttpResponse(public_keys[gid]['aes_key'])
-    else:
-        return HttpResponse(E_MSG)
 
-def add_user(request):
+def register(request):
+	if not 'gid' in request.GET.keys():
+		return HttpResponse('gid required')
+	if not 'uid' in request.GET.keys():
+		return HttpResponse('uid required')
+
 	gid = request.GET['gid']
 	uid = request.GET['uid']
-	if gid == '' or uid == '':
-		return HttpResponse(E_MSG)
 
-	authorise(uid,gid)
-	return HttpResponse('added ' + str(uid) + ' to ' + str(gid))
-
-def authorise(uid,gid):
-	if (not authorised(uid,gid)) and gid in keys.keys():
-		keys[gid]['members'].append(uid)
-
-
-def authorised(uid, gid):
-    return gid in keys.keys() and uid in keys[gid]['members']
+	if gid in keys.keys():
+		return HttpResponse(gid + ' already registered')
+	
+	keys[gid] = {'admin':uid, 'u_keys':[]}
+	return HttpResponse('registered ' + str(gid))
